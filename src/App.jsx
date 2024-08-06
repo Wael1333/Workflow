0d9f863afabf0698426cae66cb0fd4514b73869e
Wael1333 <khalfaoui1214@gmail.com>
2024-08-06 23:39:27 +0100

    Main back to work

--- src/App.jsx
+++ src/App.jsx
@@ -20,14 +20,14 @@ const App = () => {
     <Router>
       <div className="flex flex-col min-h-screen">
         <Navbar />
-        <div className="flex-grow">
+        <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/skills" element={<Skills />} />
           </Routes>
-        </div>
+        </main>
         <Footer />
       </div>
     </Router>
